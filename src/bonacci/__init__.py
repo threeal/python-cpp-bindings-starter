38c3e888071aea01827eaa11c65ec1abf97d68d3
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-09-16 15:21:15 +0800

    feat: use single package for binding (#14)
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- src/bonacci/__init__.py
+++ src/bonacci/__init__.py
@@ -1,5 +1,5 @@
 """Example Python package for generating Fibonacci sequences."""
 
-from bonacci_bindings.sequence import fibonacci_sequence
+from .sequence import fibonacci_sequence
 
 __all__ = ["fibonacci_sequence"]
